3d23b263bb83722ee248477ee41b8deaba28bc2d
ywt <3254213247@qq.com>
2026-09-14 21:15:50 +0800

    增加一些注释

--- pans/src/logger/log_level.cc
+++ pans/src/logger/log_level.cc
@@ -25,12 +25,18 @@ LogLevel::ToString(Level level) noexcept
 LogLevel::Level 
 LogLevel::FromString(std::string_view value) noexcept
 {
+    //那这个就是默认只会传类似 DEBUG 之类的string_view
     std::array<char,6> normalized{};
     std::transform(value.begin(),value.end(),normalized.begin(),
     [](unsigned char character){
         return static_cast<char>(std::toupper(character));
     });
     const std::string_view upper_value(normalized.data(), value.size());
+/*
+    switch 的条件表达式只能是整型、枚举型或可隐式转换为整型的类型。
+    字符串（std::string、
+    std::string_view、const char*）不能作为 switch 的条件。
+*/
 #define XX(name) if(upper_value == #name) return Level::LOG_LV_##name;
     XX(DEBUG)
     XX(INFO)
